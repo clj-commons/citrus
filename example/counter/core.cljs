07e0984851d530549a28ef31e3adc4d8943b366b
Roman Liutikov <roman01la@romanliutikov.com>
2017-04-30 11:57:29 +0300

    update example

--- example/counter/core.cljs
+++ example/counter/core.cljs
@@ -1,6 +1,7 @@
 (ns counter.core
   (:require [rum.core :as rum]
-            [scrum.core :as scrum]))
+            [scrum.core :as scrum]
+            [goog.dom :as dom]))
 
 ;;
 ;; define controller & action handlers
@@ -11,13 +12,34 @@
 (defmulti control (fn [action] action))
 
 (defmethod control :init []
-  initial-state)
+  {:state initial-state})
+
+(defmethod control :load [_ [key]]
+  {:local-storage {:op :get
+                   :key key
+                   :on-success :load-ready}})
+
+(defmethod control :load-ready [_ [counter]]
+  {:state (int counter)})
+
+(defmethod control :save [_ [key] counter]
+  {:local-storage {:op :set
+                   :value counter
+                   :key key}})
 
 (defmethod control :inc [_ _ counter]
-  (inc counter))
+  (let [next-counter (inc counter)]
+    {:state next-counter
+     :local-storage {:op :set
+                     :value next-counter
+                     :key :count}}))
 
 (defmethod control :dec [_ _ counter]
-  (dec counter))
+  (let [next-counter (dec counter)]
+    {:state next-counter
+     :local-storage {:op :set
+                     :value next-counter
+                     :key :count}}))
 
 
 ;;
@@ -32,17 +54,37 @@
 
 
 ;;
+;; define effects handler
+;;
+
+(defn local-storage [r c {:keys [op key value on-success]}]
+  (case op
+    :get
+    (->> (name key)
+         js/localStorage.getItem
+         (scrum/dispatch! r c on-success))
+    :set
+    (-> (name key)
+        (js/localStorage.setItem value))))
+
+
+;;
 ;; start up
 ;;
 
 ;; create Reconciler instance
 (defonce reconciler
-  (scrum/reconciler {:state (atom {})
-                     :controllers {:counter control}}))
+  (scrum/reconciler
+    {:state (atom {})
+     :controllers {:counter control}
+     :effect-handlers {:local-storage local-storage}}))
 
 ;; initialize controllers
 (defonce init-ctrl (scrum/broadcast-sync! reconciler :init))
 
+;; load from localStorage
+(defonce load (scrum/dispatch-sync! reconciler :counter :load :count))
+
 ;; render
 (rum/mount (Counter reconciler)
-           (. js/document (getElementById "app")))
+           (dom/getElement "app"))
