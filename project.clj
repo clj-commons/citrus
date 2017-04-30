08d85971623ed6a82a2357625d538c23e1b9f7f4
Roman Liutikov <roman01la@romanliutikov.com>
2017-04-30 11:57:46 +0300

    update deps & add min build

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.456" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.521" :scope "provided"]
                  [rum "0.10.8"]]
 
-  :plugins [[lein-cljsbuild "1.1.5" :exclusions [[org.clojure/clojure]]]]
+  :plugins [[lein-cljsbuild "1.1.6" :exclusions [[org.clojure/clojure]]]]
 
   :cljsbuild {:builds
               [{:id "dev"
@@ -16,5 +16,17 @@
                            :asset-path "target/dev"
                            :output-to "target/counter.js"
                            :output-dir "target/dev"
+                           :compiler-stats true
+                           :parallel-build true}}
+               {:id "min"
+                :source-paths ["src" "example"]
+                :compiler {:main counter.core
+                           :output-to "target/counter.js"
+                           :output-dir "target/min"
+                           :optimizations :advanced
+                           :closure-defines {"goog.DEBUG" false}
+                           :static-fns true
+                           :elide-asserts true
+                           :output-wrapper true
                            :compiler-stats true
                            :parallel-build true}}]})
