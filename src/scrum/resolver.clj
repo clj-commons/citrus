86d9adf44fe4e489316d61da1d686777770755bd
Roman Liutikov <roman01la@romanliutikov.com>
2017-06-05 23:33:06 +0300

    mock IRef protocol on Resolver type which is expected by rum/derived-atom

--- src/scrum/resolver.clj
+++ src/scrum/resolver.clj
@@ -10,7 +10,23 @@
         (swap! state assoc-in path data))
       (if reducer
         (reducer data)
-        data))))
+        data)))
+
+  clojure.lang.IRef
+  (setValidator [this vf]
+    (throw (UnsupportedOperationException. "scrum.resolver.Resolver/setValidator")))
+
+  (getValidator [this]
+    (throw (UnsupportedOperationException. "scrum.resolver.Resolver/getValidator")))
+
+  (getWatches [this]
+    (throw (UnsupportedOperationException. "scrum.resolver.Resolver/getWatches")))
+
+  (addWatch [this key callback]
+    this)
+
+  (removeWatch [this key]
+    this))
 
 (defn make-resolver [state resolvers path reducer]
    (Resolver. state resolvers path reducer))
