d5ce408d6cd30567a018a6418173e0a3c5b882d9
Roman Liutikov <roman01la@romanliutikov.com>
2017-12-26 17:46:51 +0200

    wip

---
+++ src/citrus/cofx.clj
@@ -0,0 +1,37 @@
+(ns citrus.cofx)
+
+(require '[clojure.spec.alpha :as s])
+
+(defn parse-defhandler [args]
+  (s/conform
+    (s/cat
+      :ctrl-name symbol?
+      :event-name keyword?
+      :meta (s/? map?)
+      :args (s/spec
+              (s/cat
+                :event some?
+                :args some?
+                :state some?
+                :cofx some?))
+      :body (s/* some?))
+    args))
+
+(defmacro defhandler [& args]
+  (let [result (parse-defhandler args)]
+    (if (not= result ::s/invalid)
+      (let [{:keys [ctrl-name event-name meta args body]} result
+            {:keys [event args state cofx]} args]
+        `(do
+           (alter-meta! #'~ctrl-name #(assoc-in % [:citrus ~event-name] ~meta))
+           (defmethod ~ctrl-name ~event-name [~event ~args ~state ~cofx] ~@body))))))
+
+(comment
+  (defmulti control (fn [event _ _ _] event))
+
+  (defhandler control :fetch-profiles
+    {:cofx {:local-store #(identity "auth-token")}}
+    [_ [account-id] state coeffects]
+    {:state (assoc state :loading? true)})
+
+  (-> (meta #'control) :citrus :fetch-profiles))
