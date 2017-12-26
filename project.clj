98ac31a2e51284a0080cbfcd6f657abccefd0922
Roman Liutikov <roman01la@romanliutikov.com>
2017-12-26 16:32:04 +0200

    3.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.roman01la/citrus "3.0.0"
+(defproject org.roman01la/citrus "3.0.1"
   :description "State management library for Rum"
   :url "https://github.com/roman01la/citrus"
   :license {:name "Eclipse Public License"
