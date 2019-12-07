8f04be93ed7b4ade4f684ae7ddc0b223c2fab56c
roman01la <roman01la@romanliutikov.com>
2019-12-07 22:49:06 +0200

    bump deps

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :url "https://github.com/clj-commons/citrus"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.439" :scope "provided"]
-                 [rum "0.11.2"]]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.597" :scope "provided"]
+                 [rum "0.11.4"]]
 
   :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
-            [lein-figwheel "0.5.15" :exclusions [org.clojure/clojure]]
+            [lein-figwheel "0.5.19" :exclusions [org.clojure/clojure]]
             [lein-doo "0.1.8"]]
 
   :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.2"]
                                   [org.clojure/tools.nrepl "0.2.12"]
                                   [binaryage/devtools "0.9.10"]
-                                  [figwheel-sidecar "0.5.15"]
+                                  [figwheel-sidecar "0.5.19"]
                                   [expound "0.7.1"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :source-paths ["src" "dev"]}}
